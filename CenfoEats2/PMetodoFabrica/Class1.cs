48081b1d27a07da5a690e2f43a9a72702a7e74e6
PaulaHelena <phernandeze@ucenfotec.ac.cr>
2023-10-10 09:30:28 -0700

    Adición carpetas y clases vacías

---
+++ CenfoEats2/PMetodoFabrica/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CenfoEats2.PMetodoFabrica
+{
+    internal class Class1
+    {
+    }
+}
