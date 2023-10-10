5184fe5245f48eafd7133424ee9dffd20dc596a6
PaulaHelena <phernandeze@ucenfotec.ac.cr>
2023-10-10 09:26:42 -0700

    Desarrollo carcasa proyecto y corrección patrón prototipo

---
+++ CenfoEats2/PPrototipo/Menu/MenuMcDonalds.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CenfoEats2.PPrototipo.Menu
+{
+    public class MenuMcDonalds : Menu
+    {
+        private string platillo1;
+        private int precioPlatillo;
+    }
+}
