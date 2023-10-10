9730ab5148d7f0e1c7c01d62390cfde3d24086ed
Andrey <lopendex@gmail.com>
2023-10-10 14:51:18 -0600

    ui de registrar usuarios

--- CenfoEats2.0/CenfoEats2.0/UI/HomePage.Designer.cs
+++ CenfoEats2.0/CenfoEats2.0/UI/HomePage.Designer.cs
@@ -28,12 +28,246 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.RegisterUser = new System.Windows.Forms.TabPage();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.gBoxRegister = new System.Windows.Forms.GroupBox();
+            this.rbClient = new System.Windows.Forms.RadioButton();
+            this.rbDriver = new System.Windows.Forms.RadioButton();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblPhoneNumber = new System.Windows.Forms.Label();
+            this.txtPhoneNumber = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.CreateRestaurant = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.RegisterUser.SuspendLayout();
+            this.gBoxRegister.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.RegisterUser);
+            this.tabControl1.Controls.Add(this.CreateRestaurant);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Location = new System.Drawing.Point(1, 4);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1059, 671);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // RegisterUser
+            // 
+            this.RegisterUser.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.RegisterUser.Controls.Add(this.btnRegister);
+            this.RegisterUser.Controls.Add(this.gBoxRegister);
+            this.RegisterUser.Location = new System.Drawing.Point(4, 22);
+            this.RegisterUser.Name = "RegisterUser";
+            this.RegisterUser.Padding = new System.Windows.Forms.Padding(3);
+            this.RegisterUser.Size = new System.Drawing.Size(1051, 645);
+            this.RegisterUser.TabIndex = 0;
+            this.RegisterUser.Text = "Registro Usuarios";
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegister.Location = new System.Drawing.Point(437, 396);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(202, 77);
+            this.btnRegister.TabIndex = 9;
+            this.btnRegister.Text = "Registrar";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // gBoxRegister
+            // 
+            this.gBoxRegister.Controls.Add(this.rbClient);
+            this.gBoxRegister.Controls.Add(this.rbDriver);
+            this.gBoxRegister.Controls.Add(this.lblPassword);
+            this.gBoxRegister.Controls.Add(this.txtPassword);
+            this.gBoxRegister.Controls.Add(this.lblPhoneNumber);
+            this.gBoxRegister.Controls.Add(this.txtPhoneNumber);
+            this.gBoxRegister.Controls.Add(this.lblEmail);
+            this.gBoxRegister.Controls.Add(this.txtEmail);
+            this.gBoxRegister.Controls.Add(this.lblName);
+            this.gBoxRegister.Controls.Add(this.txtName);
+            this.gBoxRegister.Location = new System.Drawing.Point(189, 122);
+            this.gBoxRegister.Name = "gBoxRegister";
+            this.gBoxRegister.Size = new System.Drawing.Size(720, 222);
+            this.gBoxRegister.TabIndex = 0;
+            this.gBoxRegister.TabStop = false;
+            // 
+            // rbClient
+            // 
+            this.rbClient.AutoSize = true;
+            this.rbClient.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.rbClient.Location = new System.Drawing.Point(27, 184);
+            this.rbClient.Name = "rbClient";
+            this.rbClient.Size = new System.Drawing.Size(91, 29);
+            this.rbClient.TabIndex = 10;
+            this.rbClient.TabStop = true;
+            this.rbClient.Text = "Cliente";
+            this.rbClient.UseVisualStyleBackColor = true;
+            this.rbClient.CheckedChanged += new System.EventHandler(this.rbClient_CheckedChanged);
+            // 
+            // rbDriver
+            // 
+            this.rbDriver.AutoSize = true;
+            this.rbDriver.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.rbDriver.Location = new System.Drawing.Point(403, 184);
+            this.rbDriver.Name = "rbDriver";
+            this.rbDriver.Size = new System.Drawing.Size(119, 29);
+            this.rbDriver.TabIndex = 11;
+            this.rbDriver.TabStop = true;
+            this.rbDriver.Text = "Repartidor";
+            this.rbDriver.UseVisualStyleBackColor = true;
+            this.rbDriver.CheckedChanged += new System.EventHandler(this.rbDriver_CheckedChanged);
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.lblPassword.Location = new System.Drawing.Point(398, 94);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(114, 25);
+            this.lblPassword.TabIndex = 7;
+            this.lblPassword.Text = "Contraseña";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtPassword.Location = new System.Drawing.Point(403, 122);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(288, 26);
+            this.txtPassword.TabIndex = 6;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // lblPhoneNumber
+            // 
+            this.lblPhoneNumber.AutoSize = true;
+            this.lblPhoneNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.lblPhoneNumber.Location = new System.Drawing.Point(398, 15);
+            this.lblPhoneNumber.Name = "lblPhoneNumber";
+            this.lblPhoneNumber.Size = new System.Drawing.Size(190, 25);
+            this.lblPhoneNumber.TabIndex = 5;
+            this.lblPhoneNumber.Text = "Numero de Telefono";
+            // 
+            // txtPhoneNumber
+            // 
+            this.txtPhoneNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtPhoneNumber.Location = new System.Drawing.Point(403, 43);
+            this.txtPhoneNumber.Name = "txtPhoneNumber";
+            this.txtPhoneNumber.Size = new System.Drawing.Size(288, 26);
+            this.txtPhoneNumber.TabIndex = 4;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.lblEmail.Location = new System.Drawing.Point(22, 94);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(72, 25);
+            this.lblEmail.TabIndex = 3;
+            this.lblEmail.Text = "Correo";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtEmail.Location = new System.Drawing.Point(27, 122);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(288, 26);
+            this.txtEmail.TabIndex = 2;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.lblName.Location = new System.Drawing.Point(22, 15);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(81, 25);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Nombre";
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtName.Location = new System.Drawing.Point(27, 43);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(288, 26);
+            this.txtName.TabIndex = 0;
+            // 
+            // CreateRestaurant
+            // 
+            this.CreateRestaurant.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.CreateRestaurant.Location = new System.Drawing.Point(4, 22);
+            this.CreateRestaurant.Name = "CreateRestaurant";
+            this.CreateRestaurant.Padding = new System.Windows.Forms.Padding(3);
+            this.CreateRestaurant.Size = new System.Drawing.Size(1051, 645);
+            this.CreateRestaurant.TabIndex = 1;
+            this.CreateRestaurant.Text = "Registrar Restaurantes";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(1051, 645);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Registrar Platillos";
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(1051, 645);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "tabPage4";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // HomePage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "HomePage";
+            this.ClientSize = new System.Drawing.Size(1064, 681);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "HomePage";
+            this.Text = "Pagina Inicio";
+            this.Load += new System.EventHandler(this.HomePage_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.RegisterUser.ResumeLayout(false);
+            this.gBoxRegister.ResumeLayout(false);
+            this.gBoxRegister.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage RegisterUser;
+        private System.Windows.Forms.TabPage CreateRestaurant;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblPhoneNumber;
+        private System.Windows.Forms.TextBox txtPhoneNumber;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.GroupBox gBoxRegister;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.RadioButton rbDriver;
+        private System.Windows.Forms.RadioButton rbClient;
     }
 }
--- CenfoEats2.0/CenfoEats2.0/UI/HomePage.cs
+++ CenfoEats2.0/CenfoEats2.0/UI/HomePage.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
 
 namespace CenfoEats2._0.UI
 {
@@ -16,5 +17,67 @@ namespace CenfoEats2._0.UI
         {
             InitializeComponent();
         }
+
+        private void HomePage_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            string email = txtEmail.Text;
+            string name = txtName.Text;
+            string phoneNumber = txtPhoneNumber.Text;
+            string password = txtPassword.Text;
+            string type = null;
+            if (rbClient.Checked)
+            {
+                type = "Cliente";
+            }
+            if (rbDriver.Checked)
+            {
+                type = "Repartidor";
+            }
+            if (email == null || name == null || phoneNumber == null || password == null || type == null)
+            {
+                MessageBox.Show("Por favor completar el formulario.");
+                return;
+            }
+            else
+            { 
+            string msg = $"Usuario creado \nEmail: {email}, Nombre: {name}, Teléfono: {phoneNumber},tipo: {type}";
+            MessageBox.Show(msg);
+            //var fabricaUsuarios = new Creador_Usuarios();
+            //fabricaUsuarios.CrearUsuario(name, email, phoneNumber, type, password);
+            ResetForm();
+            }
+        }
+
+        private void rbClient_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbClient.Checked)
+            {
+                rbDriver.Checked = false; 
+            }
+        }
+
+        private void rbDriver_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbDriver.Checked)
+            {
+                rbClient.Checked = false; 
+            }
+        }
+
+        private void ResetForm()
+        {
+            txtEmail.Text = "";
+            txtName.Text = "";
+            txtPhoneNumber.Text = "";
+            txtPassword.Text= "";
+            rbClient.Checked = false; 
+            rbDriver.Checked = false ;
+        }
+
     }
 }
