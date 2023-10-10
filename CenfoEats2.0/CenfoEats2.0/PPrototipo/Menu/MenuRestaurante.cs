fd069a2fb09da1f1a402baef6ba3134864d74597
PaulaHelena <phernandeze@ucenfotec.ac.cr>
2023-10-10 16:08:25 -0700

    Cambio nombres Prototipo

--- CenfoEats2.0/CenfoEats2.0/PPrototipo/Menu/MenuRestaurante.cs
+++ CenfoEats2.0/CenfoEats2.0/PPrototipo/Menu/MenuRestaurante.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 namespace CenfoEats2._0.PPrototipo.Menu
 {
-    public class MenuMcDonalds : Menu
+    public class MenuRestaurante : Menu
     {
         private string platillo1;
         private int precioPlatillo;
 
-        public MenuMcDonalds(int cantidadPlatillos, string Platillo1, int PrecioPlatillo) : base(cantidadPlatillos)
+        public MenuRestaurante(int cantidadPlatillos, string Platillo1, int PrecioPlatillo) : base(cantidadPlatillos)
         {
             platillo1 = Platillo1;
             precioPlatillo = PrecioPlatillo;
