364d4031f0eb37c8a0a749ba4bbc513289d06653
PaulaHelena <phernandeze@ucenfotec.ac.cr>
2023-11-21 10:42:17 -0800

    Clase cliente + controlador vacías

---
+++ CenfoEats2.0/CenfoEats2.0/PProxy/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CenfoEats2._0.PProxy
+{
+    public class Cliente : UsuarioDB
+    {
+
+        public string ubicacionEntrega { get; set; }
+
+
+
+    }
+}
