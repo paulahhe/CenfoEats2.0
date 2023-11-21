0b08cca9e45da5ef842ad51989dab348f7aba6de
*asalazarlcen* <*asalazarl@ucenfotec.ac.cr*>
2023-11-21 17:15:28 -0600

    Código olvidado.

--- CenfoEats2.0/CenfoEats2.0/PProxy/Implementacion/ProxyRepartidor.cs
+++ CenfoEats2.0/CenfoEats2.0/PProxy/Implementacion/ProxyRepartidor.cs
@@ -27,6 +27,99 @@ namespace CenfoEats2._0.PProxy
         private int idCliente;
 
 
+        public Usuario ObtenerInformacionCliente(int idCliente)
+        {
+            // Crear instancia del CRUD para usuarios
+            var userCrud = new UserCrudFactory();
+
+            // Obtener la lista de todos los usuarios
+            var usuarios = userCrud.RetrieveAll<UsuarioDB>();
+
+            // Buscar el usuario con el idCliente proporcionado
+            var cliente = usuarios.FirstOrDefault(u => u.id == idCliente);
+
+            // Verificar si se encontró el cliente
+            if (cliente != null)
+            {
+                // Crear una instancia de Usuario a partir de la información obtenida
+                var clienteOrden = new Cliente
+                {
+                    nombre = cliente.nombre,
+                    correo_electronico = cliente.correo_electronico,
+                    telefono = cliente.telefono,
+                    tipo = cliente.tipo,
+                    contrasenna = cliente.contrasenna
+                };
+
+                return clienteOrden;
+            }
+            else
+            {
+                // Manejar el caso en el que no se encuentre el cliente
+                throw new Exception("Cliente no encontrado");
+            }
+        }
+
+
+        public Usuario ObtenerInformacionRepartidor(int idRepartidor)
+        {
+            // Crear instancia del CRUD para usuarios
+            var userCrud = new UserCrudFactory();
+
+            // Obtener la lista de todos los usuarios
+            var usuarios = userCrud.RetrieveAll<UsuarioDB>();
+
+            // Buscar el usuario con el idRepartidor proporcionado
+            var cliente = usuarios.FirstOrDefault(u => u.id == idCliente);
+
+            // Verificar si se encontró el repartidor
+            if (cliente != null)
+            {
+                // Crear una instancia de Usuario a partir de la información obtenida
+                var clienteOrden = new Cliente
+                {
+                    nombre = cliente.nombre,
+                    correo_electronico = cliente.correo_electronico,
+                    telefono = cliente.telefono,
+                    tipo = cliente.tipo,
+                    contrasenna = cliente.contrasenna
+                };
+
+                return clienteOrden;
+            }
+            else
+            {
+                // Manejar el caso en el que no se encuentre el cliente
+                throw new Exception("Cliente no encontrado");
+            }
+        }
+
+
+
+        public void ObtenerInfoPedido(int idPedido)
+
+           
+        {
+            // Lógica para validar el acceso basado en el tipo de usuario
+            if (usuario.tipo == "Repartidor")
+            {
+                if (usuario.id == pedido.GetIdDriver())
+                {
+                    ubicacionEntrega = pedido.ubicacion;
+                    telefonoCliente = ObtenerInformacionCliente(pedido.GetIdClient()).telefono;
+
+                    
+                }
+              
+            }
+            else
+            {
+                // Manejar el caso en el que un cliente u otro tipo de usuario intenta acceder
+                // Puedes lanzar una excepción o devolver un objeto Pedido vacío, dependiendo de tus requerimientos
+                throw new UnauthorizedAccessException("Acceso no autorizado");
+            }
+        }
+
         public string ObtenerPedido(int idRepartidor, int idPedido)
         {
             throw new NotImplementedException();
