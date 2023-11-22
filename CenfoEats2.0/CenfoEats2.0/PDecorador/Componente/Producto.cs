72c209eb66efcbc20ed4bcf93a6ab93d2cbef0ac
PaulaHelena <phernandeze@ucenfotec.ac.cr>
2023-11-22 11:00:48 -0800

    Modificacion menor

--- CenfoEats2.0/CenfoEats2.0/PDecorador/Componente/Producto.cs
+++ CenfoEats2.0/CenfoEats2.0/PDecorador/Componente/Producto.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CenfoEats2._0.PPrototipo.Menu;
+using CenfoEats2._0.PPrototipo.iPrototipo;
 using CenfoEats2._0.PPrototipo.Prototipo;
 
 namespace CenfoEats2._0.PDecorador.Componente
 {
-    public class Producto
+    public class Producto : iPlatillo
     {
-        Platillo producto;
         Restaurant restaurante;
 
     }
