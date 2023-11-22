138e2bdf8bd2115326ad7dd14db82a8110026ec8
PaulaHelena <phernandeze@ucenfotec.ac.cr>
2023-11-22 10:32:06 -0800

    Prueba 1 decorador

---
+++ CenfoEats2.0/CenfoEats2.0/PDecorador/Componente/Producto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CenfoEats2._0.PPrototipo.Menu;
+using CenfoEats2._0.PPrototipo.Prototipo;
+
+namespace CenfoEats2._0.PDecorador.Componente
+{
+    public class Producto
+    {
+        Platillo producto;
+        Restaurant restaurante;
+
+    }
+}
---
+++ CenfoEats2.0/CenfoEats2.0/PDecorador/Decorador/IngredienteExtraDecorador.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CenfoEats2._0.PDecorador.Componente;
+
+
+namespace CenfoEats2._0.PDecorador.Decorador
+{
+    public abstract class IngredienteExtraDecorador : Producto
+    {
+        protected Producto platillo;
+
+
+    }
+}
