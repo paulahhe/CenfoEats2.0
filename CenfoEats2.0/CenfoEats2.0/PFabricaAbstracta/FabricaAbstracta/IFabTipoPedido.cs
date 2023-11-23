20ccc8a6857cedc56296e8a21fc31c1f84d422d1
LuisKingZX <131728936+LuisKingZX@users.noreply.github.com>
2023-11-22 18:11:19 -0600

    Corrección de método fabrica

---
+++ CenfoEats2.0/CenfoEats2.0/PFabricaAbstracta/FabricaAbstracta/IFabTipoPedido.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models.FabricaAbstracta.Producto_Abstracto;
+
+namespace Models.FabricaAbstracta.Fabrica_Abstracta
+{
+    public interface IFabTipoPedido
+    {
+       IPedido crearPedido();
+    }
+}
---
+++ CenfoEats2.0/CenfoEats2.0/PPrototipo/iPrototipo/Restaurant.cs
@@ -0,0 +1,43 @@
+﻿using CenfoEats2._0.PPrototipo.Menu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CenfoEats2._0.PPrototipo.iPrototipo
+{
+    public abstract class Restaurant
+    {
+        public string name { get; set; }
+        public string address { get; set; }
+        public int phoneNumber { get; set; }
+        public string foodType { get; set; }
+        public int restaurantRating { get; set; }
+        public List<Platillo> menu { get; set; }
+
+        public Restaurant(string name, string address, int phoneNumber, string foodType, int restaurantRating, List<Platillo> menu)
+        {
+            this.name = name;
+            this.address = address;
+            this.phoneNumber = phoneNumber;
+            this.foodType = foodType;
+            this.restaurantRating = restaurantRating;
+            this.menu = menu;
+        }
+
+        public string getData()
+        {
+            string mData = "< Sobre este producto >\n";
+
+            mData += "Nombre del restaurante: " + name + "\n";
+            mData += "Dirección: " + address + "\n";
+            mData += "Número de teléfono: " + phoneNumber + "\n";
+            mData += "Tipo de comida: " + foodType + "\n";
+            mData += "Rating  " + restaurantRating + "\n";
+            return mData;
+        }
+
+        public abstract Restaurant clone();
+    }
+}
