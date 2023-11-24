8b161f1cf32c17748dbf1868351ab4872148d0ee
Andrey <lopendex@gmail.com>
2023-11-24 16:20:35 -0600

    Error referencia

--- CenfoEats2.0/CenfoEats2.0/PObservador/Interfaces/ISujeto.cs
+++ CenfoEats2.0/CenfoEats2.0/PObservador/Interfaces/ISujeto.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CenfoEats2._0.PObservador.Concreto;
 
 namespace CenfoEats2._0.PObservador.Interfaces
 {
